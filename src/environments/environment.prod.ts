7944aa217fad9a7484725689a43828eeb944e233
vatsal <vso.opash@gmail.com>
2024-05-09 17:12:49 +0530

    Change API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   hmr: false,
-  serverUrl: 'https://api.2040.chat/api/v1/',
+  serverUrl: 'https://api.chat.buzz/api/v1/',
   serverUrl2: 'http://localhost:8080/api/v1/',
-  socketUrl: 'https://api.2040.chat/',
+  socketUrl: 'https://api.chat.buzz/',
   socketUrl2: 'http://localhost:8080/',
   imgUrl: 'root/storage/uploads',
 
