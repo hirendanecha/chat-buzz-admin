61045d8beff0d77fdde601037a13187fa9fcc402
vatsal <vso.opash@gmail.com>
2024-05-09 17:18:09 +0530

    Change wording

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { TokenStorageService } from './services/token-storage.service';
   template: '<router-outlet></router-outlet>',
 })
 export class AppComponent implements OnInit {
-  title = 'Chat Buzz Admin';
+  title = 'Chat.buzz Admin';
 
   constructor(
     private router: Router,
